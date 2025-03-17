5095f4200dfeb7056ecbce385b9bc711da9d8e55
mrphaox <oscar.neira@usantoto.edu.co>
2025-03-16 23:32:33 -0500

    v2 cors

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,7 @@ import * as dotenv from 'dotenv';
 import { NestExpressApplication } from '@nestjs/platform-express';
 import { join } from 'path';
 import { ValidationPipe } from '@nestjs/common';
+import { DocumentBuilder } from '@nestjs/swagger';
 
 dotenv.config();
 
@@ -13,6 +14,13 @@ async function bootstrap() {
 
   app.useGlobalPipes(new ValidationPipe());
   app.useStaticAssets(join(__dirname,'..','uploads'));
+
+  const config = new DocumentBuilder()
+  .setTitle('API de Inventario y Chat')
+  .setDescription('Documentación de la API con Swagger')
+  .setVersion('1.0')
+  .addBearerAuth() // Permite autenticación con JWT en Swagger
+  .build();
 
   await app.listen(port);
   console.log(`\x1b[32m[Nest] 🚀 Application is running on: http://localhost:${port}\x1b[0m`);
